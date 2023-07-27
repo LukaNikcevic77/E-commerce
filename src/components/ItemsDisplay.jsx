58d3f1faf559f2cc92af4b7af90bef5356ed7f5a
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-07-27 18:06:18 +0200

    Fixed grid a bit

--- src/components/ItemsDisplay.jsx
+++ src/components/ItemsDisplay.jsx
@@ -15,8 +15,20 @@ return (
             {items != null && items.slice(0, 100).map((item) => {
                 return <div className="item"> 
                 <img src={item.image_link} alt="" />
-                <p>{item.brand}</p> <p>{item.name}</p>
+                <div>
+                <span>
+
+                 <p>{item.name}</p>
+                <p>{item.brand}</p>
+
+                    </span>
+
+                    <span>
+
                 <p>{item.price}{item.price_sign}</p>
+                    </span>
+                </div>
+
                 </div>
             })}
 
