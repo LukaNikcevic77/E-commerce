485a4521ccca1d1079936709c967d18a36588342
LukaNikcevic77 <nikcevic.luka77@gmail.com>
2023-07-29 13:30:14 +0200

    Styled almost all elements, need to fix cart

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";  
 import { useState, useEffect, useContext } from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faMagnifyingGlass, faBagShopping} from "@fortawesome/free-solid-svg-icons"
+import {faMagnifyingGlass, faBagShopping, faArrowLeftLong} from "@fortawesome/free-solid-svg-icons"
+import { HomeContext } from "../context/HomeContext";
 import Categories from "../components/Categories";
 import ItemsDisplay from "./ItemsDisplay";
 function Home(){
@@ -9,13 +10,25 @@ return (
     <>
     <div className='topNav'>
       <FontAwesomeIcon icon={faMagnifyingGlass} className="fa" />
-        <h1>D'alma</h1>
-        <span className="ShoppingCartHolder">
+        <h1>NYX</h1>
+        <span className="ShoppingCartHolder" >
         <span className="Counter">
             <h1>5</h1>
         </span>
         <FontAwesomeIcon icon={faBagShopping} className="fa"/>
         </span>
+        <div className="cart" >
+          <div className="cart--Nav">
+            <button className="empty--button"> <FontAwesomeIcon icon={faArrowLeftLong} className="fa" /> </button>
+            <h1>Cart</h1>
+          </div>
+        <div className="cart--items"></div>
+        <span className="price--container">
+          <p>Total price:</p>
+          <h1>50$</h1>
+        </span>
+        <button className="pay--button">Pay</button>
+        </div>
       </div>
      <Categories />
      <ItemsDisplay />
--- src/components/ItemsDisplay.jsx
+++ src/components/ItemsDisplay.jsx
@@ -8,24 +8,40 @@ function ItemsDisplay(){
 
     useEffect(() => {console.log(items)}, [items])
 
+    const descriptionCutter = (a) => {
+
+        const pattern = /^(.*?[.!?])/;
+
+        const match = a.match(pattern);
+
+        const result = match ? match[1] : a;
+
+        return result;
+    }
+
 
 return (
     <>
         <div className="items-grid">
             {items != null && items.slice(0, 100).map((item) => {
                 return <div className="item"> 
-                <img src={item.image_link} alt="" />
-                <div>
-                <span>
+                <div className="left-block">
 
-                 <p>{item.name}</p>
-                <p>{item.brand}</p>
+                <img src={item.image_link} alt="" />
+                </div>
+                <div className="right-block">
+                <span className="name-desc">
+ 
+                 <p className="product--name">{item.name.toUpperCase()}</p>
+                <p className="product--desc ">{descriptionCutter(item.description)}</p>
 
                     </span>
 
                     <span>
+                <button className="add--To--Cart">
 
                 <p>{item.price}{item.price_sign}</p>
+                </button>
                     </span>
                 </div>
 
